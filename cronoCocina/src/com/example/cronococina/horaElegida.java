e207a9711200593136ce79c89cce8685909ebf18
emerrefe <mcarmenraflo@gmail.com>
2013-05-05 20:54:02 +0200

    alarma

--- cronoCocina/src/com/example/cronococina/horaElegida.java
+++ cronoCocina/src/com/example/cronococina/horaElegida.java
@@ -4,9 +4,11 @@ package com.example.cronococina;
 
 import android.app.Activity;
 //import android.app.TimePickerDialog;
+import android.media.MediaPlayer;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.util.Log;
 import android.widget.ProgressBar;
 import android.widget.TextView;
@@ -15,7 +17,7 @@ import android.widget.TextView;
 public class horaElegida extends Activity {
 	
 	private TextView tiempoRestante;
-	private ProgressBar progress;
+	private MediaPlayer alarma;
 	
 	private String horaU;
 	private String minutoU;
@@ -56,6 +58,8 @@ public class horaElegida extends Activity {
 			
 		countDownTimer.start();
 		
+		alarma = MediaPlayer.create(this,R.raw.CLOCK1);
+		
 	}
 	
 	
@@ -71,6 +75,8 @@ public class horaElegida extends Activity {
 		@Override
 		public void onFinish() {
 			tiempoRestante.setText("00");
+			alarma.start();
+			
 		}
 
 		@Override
@@ -95,116 +101,5 @@ public class horaElegida extends Activity {
 	}
 	
 	
-	/*
-	public class ProgressTask extends AsyncTask {
-
-		@Override
-		protected void onPreExecute() {
-			// initialize the progress bar
-			// Toast, popup text when the progress is begin
-			//Toast.makeText(MainActivity.this, "Progress begin",
-				//	Toast.LENGTH_LONG).show();
-			// set maximum progress to 100.
-			progress.setMax(100);
-
-		}
-
-		@Override
-		protected void onCancelled() {
-			// stop the progress back to 0
-			progress.setMax(0);
-
-		}
-
-		@Override
-		protected Void doInBackground(Integer... params) {
-			// get the initial starting value
-			int start = params[0];
-			// increment the progress
-			for (int i = start; i <= 100; i += 5) {
-				try {
-					boolean cancelled = isCancelled();
-					// if async task is not cancelled, update the progress
-					if (!cancelled) {
-						publishProgress(i);
-						// delay progress
-						SystemClock.sleep(1000);
-
-					}
-
-				} catch (Exception e) {
-					Log.e("Error", e.toString());
-				}
-
-			}
-			return null;
-		}
-
-		@Override
-		protected void onProgressUpdate(Integer... values) {
-			// increment progress bar by progress value
-			progress.setProgress(values[0]);
-		}
-
-		@Override
-		protected void onPostExecute(Void result) {
-			// async task finished
-			Log.v("Progress", "Finished");
-			// Toast, popup text when the progress is finshed
-			Toast.makeText(MainActivity.this, "Progress finished",
-					Toast.LENGTH_LONG).show();
-			// when the progress finished. the button can click again
-			buttonStartProgress.setClickable(true);
-		}
-
-		@Override
-		protected Object doInBackground(Object... arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-	}
-
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.main);
-		// call id function from main.xml
-		buttonStartProgress = (Button) findViewById(R.id.btn);
-		buttonCancel = (Button) findViewById(R.id.btnCancel);
-		progress = (ProgressBar) findViewById(R.id.progress);
-
-		// create click method function for start progress
-		buttonStartProgress.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				ProgressTask task = new ProgressTask();
-				switch (v.getId()) {
-				case R.id.btn:
-					// execute progress starting from 10 to 100
-					task.execute(10);
-					// when the progressbarr still in progress. the button cant
-					// click
-					buttonStartProgress.setClickable(false);
-					break;
-				}
-			}
-		});
-
-		// create click method function for cancel progress
-		buttonCancel.setOnClickListener(new Button.OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				ProgressTask task = new ProgressTask();
-				switch (v.getId()) {
-				case R.id.btnCancel:
-					task.cancel(true);
-					break;
-				}
-
-			}
-		});
-
-	}*/
 		
 }
