b80dc809b778deb878849488fa83e9424ef79578
MRF <MRF@MRF-wisi>
2013-05-05 14:06:11 +0200

    Segunda

--- cronoCocina/src/com/example/cronococina/MainActivity.java
+++ cronoCocina/src/com/example/cronococina/MainActivity.java
@@ -1,11 +1,11 @@
 package com.example.cronococina;
 
-import com.example.cronococina.R.id;
+//import com.example.cronococina.R.id;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
-import android.util.Log;
+//import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.TimePicker;
